34e82ad8fadcb55f297082790a4398fb163b9db6
Jonathan Zollinger <jzollinger@trivir.com>
2023-04-26 11:19:56 -0600

    Initial Commit

---
+++ src/install_eDirectory_927_container.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# installs eDirectory as a container and instantiates it for idm-unit testing
+#
+
+#######################################
+# prints error message with timestamp to stderr
+#
+# Globals:
+#   None
+# Arguments:
+#   Error message string.
+# Outputs:
+#   Error message with timestamp
+# Returns:
+#   1
+#######################################
+err() {
+  red='\033[0;31m'
+  no_color='\033[0m' # No Color
+  echo -e "${red}[ERROR $(date +'%Y-%m-%dT%H:%M:%S%z')] $*${no_color}" >&2
+  return 1
+}
+
+#######################################
+# unarchives eDirectory container image from provided tarball to the local directory. 
+#
+# Globals:
+#   None
+# Arguments:
+#   tarball filepath 
+# Outputs:
+#   unarchived container content to the pwd. 
+# Returns:
+#   0 on success, non-zero on error
+#######################################
+unarchive_edirectory_tarball() {
+  if ! [[ $# -ne 1 ]]; then
+    err "expected 1 argument, received $#"
+    return
+  elif [[ ! -e $1 ]]; then
+    err "No eDirectory tarball available."
+    return
+  elif [[ ! $(tar --version) ]]; then
+    err "the tar command isn't executable. Is it installed and on PATH?"
+    return
+  fi
+  tar -xf "$1" --strip-components=1 --exclude="*azure*" -C "${PWD}"
+}
+
+#######################################
+# loads eDirectory image
+#
+# Arguments:
+#   container tarball filepath 
+#
+# Outputs:
+#   a new eDirectory image  
+# Returns:
+#   image repo and tag on success, non-zero on error
+#######################################
+build_edir_image() {
+  if [[ ! $(docker --version) ]]; then
+    err "docker isn't executable. Is it installed and on PATH? \n\tNOTE: This container will not work with podman"
+  fi
+  if [[ ! -e $1 ]]; then
+    err "The provided file, '$1' doesn't exist."
+    return
+  fi
+
+  dockerOutput=$(docker load -i "$1")
+  if [[ $? ]]; then
+    return "${dockerOutput#Loaded image: }"
+  fi
+  return $?
+}
+
+#######################################
+# build eDir container and initialize eDir server per global variables 
+#
+# Globals:
+#   CONTAINER_NAME
+#   VOLUME
+#   SOURCE_IMAGE
+#   IP_PORT
+#   SERVER_CONTEXT
+#   SERVER_NAME
+#   TREENAME
+#   LDAP_PORT
+#   SSL_PORT
+#   HTTP_PORT
+#   HTTPS_PORT
+#
+# Outputs:
+#   new eDirectory server container  
+# Returns:
+#   image repo and tag on success, non-zero on error#
+build_edir_container() {
+  declare -A global_variables=(
+  ["CONTAINER_NAME"]=$CONTAINER_NAME
+  ["VOLUME"]=$VOLUME
+  ["SOURCE_IMAGE"]=$SOURCE_IMAGE
+  ["IP_PORT"]=$IP_PORT
+  ["SERVER_CONTEXT"]=$SERVER_CONTEXT
+  ["SERVER_NAME"]=$SERVER_NAME
+  ["TREENAME"]=$TREENAME
+  ["LDAP_PORT"]=$LDAP_PORT
+  ["SSL_PORT"]=$SSL_PORT
+  ["HTTP_PORT"]=$HTTP_PORT
+  ["HTTPS_PORT"]=$HTTPS_PORT
+)
+  for global_variable in "${!global_variables[@]}"; do
+    if [[ -z "${global_variables[$global_variable]}" ]]; then
+      err "the global variable '${global_variable}' is null." 
+    fi
+  done
+
+  docker run -it --name "${CONTAINER_NAME}" --stop-timeout 180 --restart on-failure:5 --memory="700M" --pids-limit="300" --volume "${VOLUME}":/config --network=host "${SOURCE_IMAGE}" new -t "${TREENAME}" -n "${SERVER_CONTEXT}" -S "${SERVER_NAME}" -B "${IP_PORT}" -o "${HTTP_PORT}" -O "${HTTPS_PORT}" -L "${LDAP_PORT}" -L "${SSL_PORT}" --CONFIGURE-EBA-NOW YES
+}
---
+++ test/build_eDir_container_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+export CONTAINER_NAME='eDir-container-1'
+export VOLUME='eDir-config'
+export SOURCE_IMAGE='edirectory:9.2.7'
+export IP_PORT='localhost@2524'
+export SERVER_CONTEXT='novell'
+export SERVER_NAME='IDM-unit'
+export TREENAME='trivir'
+export LDAP_PORT=1389
+export SSL_PORT=1636
+export HTTP_PORT=1028
+export HTTPS_PORT=1030
